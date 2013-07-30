643ae7b9c7ef496e9f93b495b9636be9ff5a5345
Thomas Levine <occurrence@thomaslevine.com>
2013-07-30 13:26:16 -0700

    covariance seems to work, though there is rounding error

--- covariance.r
+++ covariance.r
@@ -1,2 +1,3 @@
 library(sqldf)
-sqldf('SELECT "close_today", "open_today" FROM "t3c";', dbname = 'treasury_data.db')
+t3c <- sqldf('SELECT "close_today", "open_today" FROM "t3c";', dbname = 'treasury_data.db')
+print(cov(na.omit(t3c))[1,2])
--- covariance.sql
+++ covariance.sql
@@ -2,5 +2,6 @@ SELECT
   SUM(
     ("close_today" - (SELECT AVG("close_today") FROM "t3c")) *
     ("open_today" - (SELECT AVG("open_today") FROM "t3c"))
-  )
+  ) /
+  COUNT(*)
 FROM "t3c";
